f4d24de0fb42e9b8a3b39768f9be5dbc76015f62
Bogus Franczak <MQH7HYC@UPS.COM>
2018-11-26 16:02:21 +0100

    Complete minimal functionality

--- js/script.js
+++ js/script.js
@@ -1,32 +1,53 @@
-var playerTotal = 0;
-var computerTotal = 0;
-var scoreMaximum = 10;
+'use strict'
 
-var playerMove = function(playerMove){
-    var computerMove = computerMove();
-    var playerScore = 0;
-    var computerScore = 0;
+var computerScores = 0;
+var scoreMaximum = 10;
+var playerScores = 0;
+var rockBtn = document.getElementById('rock');
+var paperBtn = document.getElementById('paper');
+var scissorsBtn = document.getElementById('scissors');
+
+
+var playerMove = function () {
+    var playerMove = this.id;
     var roundResult;
-    if (playerMove === computerMove){
-        roundResult = 'remis'
+    var computerMove = getComputerMove();
+    if (playerMove === computerMove) {
+        roundResult = 'draw';
+    } else if (playerMove === 'rock' && computerMove === 'scissors' ||
+        playerMove === 'paper' && computerMove === 'rock' ||
+        playerMove === 'scissors' && computerMove === 'paper') {
+        roundResult = 'You won';
+        playerScores++;
+    } else {
+        roundResult = 'You lost';
+        computerScores++;
     }
-    
-    displayResults(playerScore, computerScore, roundResult);
+
+    displayResults(playerMove, computerMove, roundResult);
 }
 
-var computerMove = function() {
+var getComputerMove = function () {
     var rdmNum = Math.ceil(Math.random() * 3);
     if (rdmNum === 1) {
-        return 'paper'    
+        return 'paper'
     } else if (rdmNum === 2) {
         return 'scissors'
     } else {
-        return 'rock' 
+        return 'rock'
     }
-    
+
 }
 
-var displayScores = function(playerScore, computerScore, result) { 
+var displayResults = function (playerMove, computerMove, result) {
     var output = document.getElementById('output');
     
+    output.textContent = result.toUpperCase() + ':'
+        + ' You played ' + playerMove.toUpperCase()
+        + ', computer played '
+        + computerMove.toUpperCase();
 }
+
+paperBtn.addEventListener('click', playerMove);
+rockBtn.addEventListener('click', playerMove);
+scissorsBtn.addEventListener('click', playerMove);
