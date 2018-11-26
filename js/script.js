a915c9d2628197206949b2e5fb6f2135fe53e370
Bogus Franczak <bfranczak@gmail.com>
2018-11-26 21:19:14 +0100

    Add new game button and functionality

--- js/script.js
+++ js/script.js
@@ -3,28 +3,34 @@
 var computerScores = 0;
 var scoreMaximum = 10;
 var playerScores = 0;
+var output = document.getElementById('output');
+var scores = document.getElementById('scores');
 var rockBtn = document.getElementById('rock');
 var paperBtn = document.getElementById('paper');
 var scissorsBtn = document.getElementById('scissors');
-
+var newGameBtn = document.getElementById('new-game');
 
 var playerMove = function () {
     var playerMove = this.id;
     var roundResult;
     var computerMove = getComputerMove();
-    if (playerMove === computerMove) {
-        roundResult = 'draw';
-    } else if (playerMove === 'rock' && computerMove === 'scissors' ||
-        playerMove === 'paper' && computerMove === 'rock' ||
-        playerMove === 'scissors' && computerMove === 'paper') {
-        roundResult = 'You won';
-        playerScores++;
-    } else {
-        roundResult = 'You lost';
-        computerScores++;
-    }
 
-    displayResults(playerMove, computerMove, roundResult);
+    if (playerScores === scoreMaximum || computerScores === scoreMaximum) {
+        finishGame();
+    } else {
+        if (playerMove === computerMove) {
+            roundResult = 'draw';
+        } else if (playerMove === 'rock' && computerMove === 'scissors' ||
+            playerMove === 'paper' && computerMove === 'rock' ||
+            playerMove === 'scissors' && computerMove === 'paper') {
+            roundResult = 'You won';
+            playerScores++;
+        } else {
+            roundResult = 'You lost';
+            computerScores++;
+        }
+        displayResults(playerMove, computerMove, roundResult);
+    }
 }
 
 var getComputerMove = function () {
@@ -40,9 +46,6 @@ var getComputerMove = function () {
 }
 
 var displayResults = function (playerMove, computerMove, result) {
-    var output = document.getElementById('output');
-    var scores = document.getElementById('scores');
-
     output.textContent = result.toUpperCase() + ':'
         + ' You played ' + playerMove.toUpperCase()
         + ', computer played '
@@ -50,6 +53,18 @@ var displayResults = function (playerMove, computerMove, result) {
     scores.textContent = playerScores + '-' + computerScores;
 }
 
+function startGame() {
+    playerScores = 0;
+    computerScores = 0;
+    scores.textContent = playerScores + ' - ' + computerScores;
+    output.textContent = '';
+}
+
+function finishGame() {
+    output.innerHTML += playerScores > computerScores ? '<br> You won the entire game!' : '<br> You lost entire game!'
+}
+
 paperBtn.addEventListener('click', playerMove);
 rockBtn.addEventListener('click', playerMove);
 scissorsBtn.addEventListener('click', playerMove);
+newGameBtn.addEventListener('click', startGame);
