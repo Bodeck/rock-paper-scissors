410b72c9a6f4ea68ba1fa0b60638450f8d1fafe8
Bogus Franczak <MQH7HYC@UPS.COM>
2019-01-03 17:03:27 +0100

    Assign events to button with loop

--- js/script.js
+++ js/script.js
@@ -5,32 +5,33 @@ var playerScores = 0;
 var maxScores = 10;
 var output = document.getElementById('output');
 var scores = document.getElementById('scores');
-var rockBtn = document.getElementById('rock');
-var paperBtn = document.getElementById('paper');
-var scissorsBtn = document.getElementById('scissors');
 var newGameBtn = document.getElementById('new-game');
 var maxScoreDisp = document.getElementById('max-scores');
 
 setDefaults();
-
-function playerMove(event) {
-    var userMove;
+var playerButtons = document.querySelectorAll('.player-move');
+playerButtons.forEach(function (button) {
+    button.addEventListener('click', function(event){
+        var move = event.currentTarget.getAttribute('data-user-move');
+        //alternative: var move = event.currentTarget.dataset.userMove;
+        playerMove(move);
+    });
+})
+function playerMove(move) {
     var roundResult;
     var computerMove;
-
     if (playerScores === maxScores || computerScores === maxScores) {
         finishGame();
     } else {
-        userMove = event.currentTarget.dataset.userMove;
         computerMove = getComputerMove();
-        roundResult = checkRoundResults(userMove, computerMove);
-        displayResults(userMove, computerMove, roundResult);
+        roundResult = checkRoundResults(move, computerMove);
+        displayResults(move, computerMove, roundResult);
         displayScores();
     }
 }
 
 function getComputerMove() {
-    return ['paper','scissors','rock'][parseInt(Math.random() * 3)];
+    return ['paper', 'scissors', 'rock'][parseInt(Math.random() * 3)];
 }
 
 function displayResults(userMove, computerMove, result) {
@@ -77,13 +78,10 @@ function finishGame() {
     output.append(par);
 }
 
-function setDefaults () {
+function setDefaults() {
     displayScores();
     maxScoreDisp.textContent = maxScores;
     output.textContent = 'Click one of buttons to start game';
 }
 
-paperBtn.addEventListener('click', playerMove);
-rockBtn.addEventListener('click', playerMove);
-scissorsBtn.addEventListener('click', playerMove);
 newGameBtn.addEventListener('click', startGame);
